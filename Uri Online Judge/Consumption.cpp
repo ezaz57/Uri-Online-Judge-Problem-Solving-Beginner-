e177b141cc063cc7c60dd8f627363967254aeeb0
ezaz57 <105972352+ezaz57@users.noreply.github.com>
2022-05-26 17:52:40 +0600

    Create Consumption.cpp

---
+++ Uri Online Judge/Consumption.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+   int X;
+   float Y;
+   
+   cin>>X>>Y;
+   cout<<fixed<<setprecision(3);
+   float ans = X/Y;
+   cout <<ans<<" km/l"<<endl;
+   
+}
