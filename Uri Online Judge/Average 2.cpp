1274b8df138e06b9e4fac5808b41d2df169df809
ezaz57 <105972352+ezaz57@users.noreply.github.com>
2022-05-26 15:35:12 +0600

    Create Average 2.cpp

---
+++ Uri Online Judge/Average 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ /
+     double a,b,c;
+     cin>>a>>b>>c;
+     double average = (a*2+b*3+c*5)/10.0;
+     cout<<"MEDIA = "<<fixed<<setprecision(1)<<average<<endl;
+ 
+    return 0;
+}
