bb7cdb0734128b1b0b481eabaca8dd5fa11b2012
ezaz57 <105972352+ezaz57@users.noreply.github.com>
2022-05-26 15:32:40 +0600

    Create Simple Product.cpp

---
+++ Uri Online Judge/Simple Product.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    int PROD;
+    int n,m;
+    
+    cin>>n>>m;
+    
+    PROD = n*m;
+    
+    cout<<"PROD = "<<PROD<<endl;
+    return 0;
+}
