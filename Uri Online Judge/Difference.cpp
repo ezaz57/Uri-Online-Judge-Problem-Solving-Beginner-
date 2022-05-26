18098b192c7395a4173c6f6264414d1260bde52c
ezaz57 <105972352+ezaz57@users.noreply.github.com>
2022-05-26 15:35:56 +0600

    Create Difference.cpp

---
+++ Uri Online Judge/Difference.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int a,b,c,d;
+     int de;
+    cin>>a>>b>>c>>d;
+   de =  (a * b - c * d);
+    cout << "DIFERENCA = "<<de<<endl;
+ 
+    return 0;
+}
