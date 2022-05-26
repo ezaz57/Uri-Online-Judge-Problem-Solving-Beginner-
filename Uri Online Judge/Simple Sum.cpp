caa9e8eb96ded795ae3e7e131ad5c3953c5f7e30
ezaz57 <105972352+ezaz57@users.noreply.github.com>
2022-05-26 15:07:00 +0600

    Create Simple Sum.cpp

---
+++ Uri Online Judge/Simple Sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int n,m;
+    int sum = 0;
+    
+    cin>>n>>m;
+    sum = n+m;
+    
+    cout<<"SOMA = "<<sum<<endl;
+
+    return 0;
+}
