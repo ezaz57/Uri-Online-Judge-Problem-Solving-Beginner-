b59d21c08342dee3dc0f176fd9158258501a98f4
ezaz57 <105972352+ezaz57@users.noreply.github.com>
2022-05-26 18:10:06 +0600

    Create Distance.cpp

---
+++ Uri Online Judge/Distance.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    
+    int result = (x*2);
+    
+    cout <<result <<" minutos"<<endl;
+    return 0;
+}
