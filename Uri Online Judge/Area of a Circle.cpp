6ab003ffd192e871b6a2c11d2cdbcb9470493b8d
ezaz57 <105972352+ezaz57@users.noreply.github.com>
2022-05-26 15:05:31 +0600

    Create Area of a Circle.cpp

---
+++ Uri Online Judge/Area of a Circle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+     
+    double r,a,pi;
+    pi =3.14159;
+    
+    cin>>r;
+    a = pi*r*r;
+    
+    
+    cout<<"A="<<fixed<<setprecision(4)<<a<<endl;
+    return 0;
+}
