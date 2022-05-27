0c750b11662425b6f4e4439a59d7b2fbde6cac7a
ezaz57 <105972352+ezaz57@users.noreply.github.com>
2022-05-27 10:14:24 +0600

    Create Fuel Spent.cpp

---
+++ Uri Online Judge/Fuel Spent.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  float  x,y,ans;
+  cin>>x>>y;
+  
+  ans = (x*y)/12.0;
+  cout <<fixed;
+  cout<<setprecision(3);
+  cout<<ans;
+  return 0;
+    
+}
